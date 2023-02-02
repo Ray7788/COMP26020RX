e98e79ce9adb563cc36039715b8e9ed552204e97
Ray7788 <ray778@foxmail.com>
2023-02-02 14:00:13 +0000

    Unfinish Exercise1

--- comp26020p2-lab3/submission.hs
+++ comp26020p2-lab3/submission.hs
@@ -1,10 +1,18 @@
 -- initialise colors and data type
 data Color = Black | White deriving (Eq, Show)
-data Quadtree = Cell Color | Grid Quadtree Quadtree Quadtree Quadtree deriving (Eq, Show)
+
+-- implement QuadTree (the order of quadrant is same as mathematical coordinate system, with top-right grid being the first quadrant)
+data QuadTree = Cell Color | Grid QuadTree QuadTree QuadTree QuadTree deriving (Eq, Show)
 
 -- 
-allBlack :: Int -> Quadtree
+allBlack :: Int -> QuadTree
 allBlack _ = Cell Black
 
-allWhite :: Int -> Quadtree
+allWhite :: Int -> QuadTree
 allWhite _ = Cell White
+
+clockwise :: QuadTree -> QuadTree -> QuadTree -> QuadTree -> QuadTree
+clockwise a b c d = Grid a b c d
+
+anticlockwise :: QuadTree -> QuadTree -> QuadTree -> QuadTree -> QuadTree
+anticlockwise a b c d = Grid d c b a
