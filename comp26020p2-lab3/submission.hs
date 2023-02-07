cb6fd4d3f33fce6a8d0597afe593a9ff7ba7b467
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-02-07 12:06:31 +0000

    Complete E2

--- comp26020p2-lab3/submission.hs
+++ comp26020p2-lab3/submission.hs
@@ -2,206 +2,101 @@
 data Color = Black | White deriving (Eq, Show)
 
 -- implement QuadTree (the order of quadrant is same as mathematical coordinate system, with top-right grid being the first quadrant)
-data QuadTree = Cell Color | Grid QuadTree QuadTree QuadTree QuadTree deriving (Eq, Show)
+data QuadTree = Cell Color Int | Grid QuadTree QuadTree QuadTree QuadTree deriving (Eq, Show)
 
--- Exercise 1
+-- returns a Black or White cell of a specified size (size could be ignored in Exercise1)
 allBlack :: Int -> QuadTree
-allBlack _ = Cell Black
+allBlack size = Cell Black size
 
 allWhite :: Int -> QuadTree
-allWhite _ = Cell White
+allWhite size = Cell White size
 
+-- Exercise 1
 clockwise :: QuadTree -> QuadTree -> QuadTree -> QuadTree -> QuadTree
 clockwise a b c d = Grid a b c d
 
 anticlockwise :: QuadTree -> QuadTree -> QuadTree -> QuadTree -> QuadTree
 anticlockwise a b c d = Grid a d c b
 
--- CAUTION: a is top-right. b is top-left, c is bottom-left. d is bottom-right --
-
 -- Exercise 2
-data QuadTreeWN = CellN Color [Color] | GridN QuadTreeWN QuadTreeWN QuadTreeWN QuadTreeWN deriving (Eq, Show)
+-- output the size of a QuadTree by calculating the length of one side (top)
+getTreeSize :: QuadTree -> Double
+getTreeSize (Cell ce size) = fromIntegral size
+getTreeSize (Grid a b c d) = getTreeSize a + getTreeSize b
 
--- stores surrounding QuadTrees of a QuadTree
-data Surrounding = Empty | SubQuadTree QuadTree deriving (Eq, Show)
-data Surroundings = Surroundings {
-    top :: Surrounding, bottom :: Surrounding,
-    left :: Surrounding, right :: Surrounding,
-    topLeft :: Surrounding, topRight :: Surrounding,     -- corner cases
-    bottomLeft :: Surrounding,  bottomRight :: Surrounding      -- corner cases
-} deriving (Eq, Show)
+-- detect one specified side of a certain Gird/Cell and sum its values depends on colors
+side :: Maybe QuadTree -> String -> Int
+side Nothing _ = 0
+-- calculate the weight of the Cell
+side (Just (Cell ce _)) _
+  | ce == White = 1 -- White cells are worth +1
+  | ce == Black = -1 -- black cells are worth -1
+  --  continue to implement recursion for Grid if cell is not arrived
+side (Just (Grid a b c d)) direction
+  | direction == "top" = (side (Just a) "top") + (side (Just b) "top")
+  | direction == "bottom" = (side (Just c) "bottom") + (side (Just d) "bottom")
+  | direction == "left" = (side (Just b) "left") + (side (Just c) "left")
+  | direction == "right" = (side (Just a) "right") + (side (Just d) "right")
 
--- define helper functions to get the specific sub-QuadTree of a QuadTree
-getGridA :: Surrounding -> Surrounding
-getGridA Empty = Empty
-getGridA (SubQuadTree (Cell ce)) = SubQuadTree (Cell ce)
-getGridA (SubQuadTree (Grid a b c d)) = SubQuadTree a
+--
+getGrid :: QuadTree -> Double -> (Double, Double, Double) -> (Double, Double, Double) -> Maybe QuadTree
+getGrid (Cell ce cellSize) _ _ _ = Just (Cell ce cellSize)
+getGrid (Grid a b c d) treeSize (x_now, y_now, sizeNow) (grid_x, grid_y, grid_size)
+  | grid_x < 0.0 || grid_x > (treeSize - 0.5) || grid_y < 0.0 || grid_y > (treeSize - 0.5) = Nothing
+  | sizeNow == grid_size = Just (Grid a b c d)
+  | x_now > grid_x && y_now > grid_y = getGrid b treeSize (x_now - offset, y_now - offset, sizeNow / 2) (grid_x, grid_y, grid_size)
+  | x_now < grid_x && y_now > grid_y = getGrid a treeSize (x_now + offset, y_now - offset, sizeNow / 2) (grid_x, grid_y, grid_size)
+  | x_now > grid_x && y_now < grid_y = getGrid c treeSize (x_now - offset, y_now + offset, sizeNow / 2) (grid_x, grid_y, grid_size)
+  | x_now < grid_x && y_now < grid_y = getGrid d treeSize (x_now + offset, y_now + offset, sizeNow / 2) (grid_x, grid_y, grid_size)
+  where
+    offset = sizeNow / 4
 
-getGridB :: Surrounding -> Surrounding
-getGridB Empty = Empty
-getGridB (SubQuadTree (Cell ce)) = SubQuadTree (Cell ce)
-getGridB (SubQuadTree (Grid a b c d)) = SubQuadTree b
+--  returns the Quadtree representing one of his four direct neighbors (top, bottom, left, right)
+-- (initial QuadTree, initial QuadTree size) -> Cell's 
+getNeighbor :: String -> (QuadTree, Double) -> (Double, Double, Double) -> Maybe QuadTree
+getNeighbor direction (fullTree, treeSize) (x_cell, y_cell, cellSize)
+  | direction == "top" = getGrid fullTree treeSize (tree_center, tree_center, treeSize) (x_cell, y_cell - cellSize, cellSize) -- top
+  | direction == "bottom" = getGrid fullTree treeSize (tree_center, tree_center, treeSize) (x_cell, y_cell + cellSize, cellSize) -- bottom
+  | direction == "left" = getGrid fullTree treeSize (tree_center, tree_center, treeSize) (x_cell - cellSize, y_cell, cellSize) -- left
+  | direction == "right" = getGrid fullTree treeSize (tree_center, tree_center, treeSize) (x_cell + cellSize, y_cell, cellSize) -- right
+  where
+    tree_center = treeSize / 2
 
-getGridC :: Surrounding -> Surrounding
-getGridC Empty = Empty
-getGridC (SubQuadTree (Cell ce)) = SubQuadTree (Cell ce)
-getGridC (SubQuadTree (Grid a b c d)) = SubQuadTree c
+-- get neighbors weight and sum the total weight of a cell's all neighbors
+-- (initial QuadTree, initial QuadTree size) -> (Neighbor Cell's X coordinate, Cell's Y coordinate, Cell's size)
+getNeighborWeight :: (QuadTree, Double) -> (Double, Double, Double) -> Int
+getNeighborWeight (fullTree, treeSize) (x_cell, y_cell, cellSize) =
+  side (getNeighbor "top" (fullTree, treeSize) (x_cell, y_cell, cellSize)) "bottom"
+    + side (getNeighbor "bottom" (fullTree, treeSize) (x_cell, y_cell, cellSize)) "top"
+    + side (getNeighbor "left" (fullTree, treeSize) (x_cell, y_cell, cellSize)) "right"
+    + side (getNeighbor "right" (fullTree, treeSize) (x_cell, y_cell, cellSize)) "left"
 
-getGridD :: Surrounding -> Surrounding
-getGridD Empty = Empty
-getGridD (SubQuadTree (Cell ce)) = SubQuadTree (Cell ce)
-getGridD (SubQuadTree (Grid a b c d)) = SubQuadTree d
+-- True engine for the programme
+-- Current QuadTree -> (initial QuadTree, initial QuadTree size) -> (Current x, y, Current QuadTree size) -> output
+blurMini :: QuadTree -> (QuadTree, Double) -> (Double, Double, Double) -> QuadTree
+-- for Cell
+blurMini (Cell c cellSize) (fullTree, treeSize) (x_now, y_now, _)
+  | c == White && (neighborWeight < 0) = Cell Black cellSize
+  | c == Black && (neighborWeight > 0) = Cell White cellSize
+  | otherwise = Cell c cellSize
+  where
+    neighborWeight = getNeighborWeight (fullTree, treeSize) (x_now, y_now, fromIntegral cellSize)
+-- for Grid
+blurMini (Grid a b c d) (fullTree, treeSize) (x_now, y_now, sizeNow) =
+  clockwise
+    (blurMini a (fullTree, treeSize) (x_now + offset, y_now - offset, sizeNow / 2))
+    (blurMini b (fullTree, treeSize) (x_now - offset, y_now - offset, sizeNow / 2))
+    (blurMini c (fullTree, treeSize) (x_now - offset, y_now + offset, sizeNow / 2))
+    (blurMini d (fullTree, treeSize) (x_now + offset, y_now + offset, sizeNow / 2))
+  where
+    offset = sizeNow / 4
 
--- define helper functions to get cells of a QuadTree in a given direction, e.g. `getTopCells` gives cells that compose the top edge of a QuadTree
-getTopCells :: Surrounding -> [Color]
-getTopCells Empty = []
-getTopCells (SubQuadTree (Cell ce)) = [ce]
-getTopCells (SubQuadTree (Grid a b c d)) = getTopCells (SubQuadTree b) ++ getTopCells (SubQuadTree a)
+-- Original Engine of whole programme
+-- use blurMini to implement the tree
+blur :: QuadTree -> QuadTree
+blur fullTree = let treeSize = (getTreeSize fullTree)
+  in (blurMini fullTree (fullTree, treeSize) (treeSize / 2, treeSize / 2, treeSize))
 
-getBottomCells :: Surrounding -> [Color]
-getBottomCells Empty = []
-getBottomCells (SubQuadTree (Cell ce)) = [ce]
-getBottomCells (SubQuadTree (Grid a b c d)) = getBottomCells (SubQuadTree c) ++ getBottomCells (SubQuadTree d)
+--   where treeSize = getTreeSize fullTree
 
-getLeftCells :: Surrounding -> [Color]
-getLeftCells Empty = []
-getLeftCells (SubQuadTree (Cell ce)) = [ce]
-getLeftCells (SubQuadTree (Grid a b c d)) = getLeftCells (SubQuadTree b) ++ getLeftCells (SubQuadTree c)
-
-getRightCells :: Surrounding -> [Color]
-getRightCells Empty = []
-getRightCells (SubQuadTree (Cell ce)) = [ce]
-getRightCells (SubQuadTree (Grid a b c d)) = getRightCells (SubQuadTree a) ++ getRightCells (SubQuadTree d)
-
-getTopLeftCell :: Surrounding -> [Color]
-getTopLeftCell Empty = []
-getTopLeftCell (SubQuadTree (Cell ce)) = [ce]
-getTopLeftCell (SubQuadTree (Grid a b c d)) = getTopLeftCell (SubQuadTree b)
-
-getTopRightCell :: Surrounding -> [color]
-getTopRightCell Empty = []
-getTopRightCell (SubQuadTree (Cell ce)) = [ce]
-getTopRightCell (SubQuadTree (Grid a b c d)) = getTopRightCell (SubQuadTree a)
-
-getBottomLeftCell :: Surrounding -> [Color]
-getBottomLeftCell Empty = []
-getBottomLeftCell (SubQuadTree (Cell ce)) = [ce]
-getBottomLeftCell (SubQuadTree (Grid a b c d)) = getBottomLeftCell (SubQuadTree c)
-
-getBottomRightCell :: Surrounding -> [Color]
-getBottomRightCell Empty = []
-getBottomRightCell (SubQuadTree (Cell ce)) = [ce]
-getBottomRightCell (SubQuadTree (Grid a b c d)) = getBottomRightCell (SubQuadTree d)
-
--- update border cells of given surroundings into neighbour list of each cell (recursion from root to leaves)
-updateNeighbours :: QuadTreeWN -> Surroundings -> QuadTreeWN
-updateNeighbours (CellWN ce neigs) surr = CellWN ce (neigs ++
-        getBottomCells (top surr) ++
-        getTopCells (bottom surr) ++
-        getRightCells (left surr) ++
-        getLeftCells (right surr) ++
-        getBottomRightCell (topLeft surr) ++
-        getBottomLeftCell (topRight surr) ++
-        getTopRightCell (bottomLeft surr) ++
-        getTopLeftCell (bottomRight surr)
-    )
-
--- pass specific sub-QuadTree of given surrounding QuadTree to form use sub-Surroundings
-updateNeighbours (GridWN a b c d) surr = GridWN
-    (updateNeighbours a (Surroundings {
-        top = getGridD (top surr),
-        bottom = Empty,
-        left = Empty,
-        right = getGridB (right surr),
-        topLeft = getGridC (top surr),
-        topRight = getGridC (topRight surr),
-        bottomLeft = Empty,
-        bottomRight = getGridC (right surr)
-    }))
-    (updateNeighbours b (Surroundings {
-        top = getGridC (top surr),
-        bottom = Empty,
-        left = getGridA (left surr),
-        right = Empty,
-        topLeft = getGridD (topLeft surr),
-        topRight = getGridD (top surr),
-        bottomLeft = getGridD (left surr),
-        bottomRight = Empty
-    }))
-    (updateNeighbours c (Surroundings {
-        top = Empty,
-        bottom = getGridB (bottom surr),
-        left = getGridD (left surr),
-        right = Empty,
-        topLeft = getGridA (left surr),
-        topRight = Empty,
-        bottomLeft = getGridA (bottomLeft surr),
-        bottomRight = getGridA (bottom surr)
-    }))
-    (updateNeighbours d (Surroundings {
-        top = Empty,
-        bottom = getGridA (bottom surr),
-        left = Empty,
-        right = getGridC (right surr),
-        topLeft = Empty,
-        topRight = getGridB (right surr),
-        bottomLeft = getGridB (bottom surr),
-        bottomRight = getGridB (bottomRight surr)
-    }))
-
--- compute neighbour list of each cell (from leaves to root)
-computeNeighbours :: QuadTree -> QuadTreeWN
-computeNeighbours (Cell ce) = CellWN ce []
-
--- pass surroundings at each level to updateNeighbours
-computeNeighbours (Grid a b c d) = GridWN
-    (updateNeighbours (computeNeighbours a) (Surroundings {
-        top = Empty,
-        bottom = SubQuadTree d,
-        left = SubQuadTree b,
-        right = Empty,
-        topLeft = Empty,
-        topRight = Empty,
-        bottomLeft = SubQuadTree c,
-        bottomRight = Empty
-    }))
-    (updateNeighbours (computeNeighbours b) (Surroundings {
-        top = Empty,
-        bottom = SubQuadTree c,
-        left = Empty,
-        right = SubQuadTree a,
-        topLeft = Empty,
-        topRight = Empty,
-        bottomLeft = Empty,
-        bottomRight = SubQuadTree d
-    }))
-    (updateNeighbours (computeNeighbours c) (Surroundings {
-        top = SubQuadTree b,
-        bottom = Empty,
-        left = Empty,
-        right = SubQuadTree d,
-        topLeft = Empty,
-        topRight = SubQuadTree a,
-        bottomLeft = Empty,
-        bottomRight = Empty
-    }))
-    (updateNeighbours (computeNeighbours d) (Surroundings {
-        top = SubQuadTree a,
-        bottom = Empty,
-        left = SubQuadTree c,
-        right = Empty,
-        topLeft = SubQuadTree b,
-        topRight = Empty,
-        bottomLeft = Empty,
-        bottomRight = Empty
-    }))
-
-
--- generate resulting matrix according to information in neighbour lists, which computes edges
-detectEdges :: QuadTreeWN -> QuadTree
-detectEdges (CellWN ce neigs) = if isSameColor ce neigs then allWhite 1 else allBlack 1
-detectEdges (GridWN a b c d) = Grid (detectEdges a) (detectEdges b) (detectEdges c) (detectEdges d)
--- my crude edge detector
-ndiff :: QuadTree -> QuadTree
-ndiff (Cell _) = allWhite 1
-ndiff grid = detectEdges (computeNeighbours grid)
+main = print (clockwise (allBlack 1) (allBlack 1) (allWhite 1) (allWhite 1) /= anticlockwise (allBlack 1) (allBlack 1) (allWhite 1) (allBlack 1))
