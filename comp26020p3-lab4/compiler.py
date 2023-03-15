a3a29f0913a93c1365648cf554ace2b21ca5f256
Ray7788 <ray778@foxmail.com>
2023-03-15 17:45:28 +0000

    Final version

--- comp26020p3-lab4/compiler.py
+++ comp26020p3-lab4/compiler.py
@@ -1,91 +1,98 @@
 import sys
-    
+
 class Graph:
-    def __init__(self, node_num, edges):
-        # initialise Graph 
+    def __init__(self, node_num,  edges):
+        # initialise a 2D list Graph 
         self.neighbors = [[] for _ in range(node_num + 1)]
+        # add neighbor node for each other
         for a, b in edges:
             self.neighbors[a].append(b)
             self.neighbors[b].append(a)
 
         # save each node's neighbor 
         self.sub_neighbors_len = [len(neighbor) for neighbor in self.neighbors]
-
         # Rank nodes (that is, live ranges) of the interference graph according to the number of neighbours in descending order.
-        self.ordered = sorted(range(len(self.sub_neighbors_len)), key= lambda k: (self.sub_neighbors_len[k], -k), reverse = True)
-
-        # pop out the last line 
+        self.ordered = sorted(range(len(self.sub_neighbors_len)), key= lambda x: (self.sub_neighbors_len[x], -x), reverse = True)
+        # pop out the node with the fewest neighbor 
         self.ordered.pop()
 
 def coloring(graph):
     # set up an empty dictionary
-    nodes_colors = {}
-    # Iterate
-    for u in graph.ordered:
-        current_colors = set()
+    colors_of_nodes = {}
+
+    for k in graph.ordered:
+        # unique elements stored in it
+        temp_colors = set()
 
         # check if it is colored
-        for i in graph.neighbors[u]:
-            if i in nodes_colors:
-                current_colors.add(nodes_colors.get(i))
+        for i in graph.neighbors[k]:
+            if i in colors_of_nodes:
+                temp_colors.add(colors_of_nodes.get(i))
 
         # find the 1st which is uncolored 
-        selected_color = 0
-        for color in current_colors:
-            if selected_color != color:
+        assigned_color = 0
+        for color in temp_colors:
+            if assigned_color != color:
                 break
-            selected_color += 1
+            assigned_color += 1
 
         # assign colors to 
-        nodes_colors[u] = selected_color
- 
-    # 打印节点颜色（调试用）
-    # for v in range(len(nodes_colors)):
-    #     print("{}{}".format(v + 1, colors[nodes_colors[v + 1]]))
+        colors_of_nodes[k] = assigned_color
+    
+    # print(len(graph.ordered), len(set(colors_of_nodes.values())))
+    if len(graph.ordered) >= 27 and len(set(colors_of_nodes.values())) > 26:
+        print("*******Cannot color the graph using 26 or fewer colors in this situation!*******")
+        sys.exit(1)
 
-    return nodes_colors
+    return colors_of_nodes
 
+# check input arguments
+if len(sys.argv) != 3:
+    print("*******Input wrong arguments!*******")
+    sys.exit(1) 
+# read input and output files' names
+input_file, output_file = sys.argv[1], sys.argv[2]
 try:
-    # read input and output files' names
-    input_file, output_file = sys.argv[1], sys.argv[2]
-    try:
-        # read the input and clean the source data
-        with open(input_file, 'r') as reader:
-            lines = [line.strip().split()[1:] for line in reader.readlines()]
-            reader.close()
+    # read the input and clean the source data
+    with open(input_file, 'r') as reader:
+        lines = [line.strip().split()[1:] for line in reader.readlines()]
+        reader.close()
 
-            # calculate how many nodes
-            node_num = len(lines)
-            edges = []
-            for i in range(node_num):
-                for j in range(len(lines[i])):
-                    edges.append((i+1, int(lines[i][j])))
+        # calculate how many nodes
+        node_num = len(lines)
+        edges = []
+        for i in range(node_num):
+            for j in range(len(lines[i])):
+                # append a tuple for each node
+                edges.append((i+1, int(lines[i][j])))
 
-            # draw the graph
-            graph = Graph(node_num, edges)
-            # shade the color
-            nodes_colors = coloring(graph)
-    except FileNotFoundError:
-        print("*******Can not find this file!*******")
-        
-except IndexError:
-    print("Input wrong")
+        # draw the graph
+        graph = Graph(node_num, edges)
+        # color the graph
+        colors_of_nodes = coloring(graph)
 
+except FileNotFoundError:
+    print("*******Can not find this file!*******")
+    sys.exit(1)    
 
+# encapsulate
 # write into output file
 try:
     with open(output_file, 'w') as writer:
-        for m in range(1, len(nodes_colors)+1):
+        for m in range(1, len(colors_of_nodes)+1):
             # deal with the last line's space
-            ending = '' if m == len(nodes_colors) else '\n'
-            print("{}{}".format(m, chr(ord('A') + nodes_colors[m])), file= writer, end= ending)
+            ending = '' if m == len(colors_of_nodes) else '\n'
+            
+            letter = chr(ord('A') + colors_of_nodes[m])
+            # Check if the character is not an alphabet   
+            if not letter.isalpha():
+                print("*******Input wrong characters!*******")
+                sys.exit(1)
+            else:
+                print("{}{}".format(m, letter), file= writer, end= ending)
     writer.close()
     print("============Output successfully!============")
+
 except:
     print("*******Can not write into file!*******")
-
-
-# eedges =[]
-# for i in range(10):
-#     for j in range(50):
-#         eedges.append((i, int(lines[i][j])))
+    sys.exit(1)    
